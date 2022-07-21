17c43a3ab6b841ec0542d14a65df1238130e9358
JJiWoNing <jjiwoning12@gmail.com>
2022-07-21 19:33:29 +0900

    7/21 Study jdbc crud

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -32,6 +33,37 @@ public class MemberRepositoryV0 {
             // jdbc 연결 끊어줘야 된다.
             close(con, pstmt, null);
         }
+    }
+
+    public Member findByid(String memberId) throws SQLException {
+        String sql = "Select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if(rs.next()){ // rs.next()로 값을 한 번 받아야 실제 데이터에 접근 할 수 있다.
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+
+        }catch (SQLException e){
+            log.info("DB error", e);
+            throw e;
+        }finally {
+            close(con, pstmt, rs);
+        }
+
     }
 
     private void close(Connection con, Statement stmt, ResultSet rs){
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,20 +1,30 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("v0", 10000);
+        //save test
+        Member member = new Member("v1", 10000);
         repositoryV0.save(member);
+
+        //findById test
+        Member findMember = repositoryV0.findByid(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 
 }
