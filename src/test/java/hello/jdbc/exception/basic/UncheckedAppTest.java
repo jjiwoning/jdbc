ab3af4a946eb56764a558e0335251f9babde118d
JJiWoNing <jjiwoning12@gmail.com>
2022-07-28 23:56:04 +0900

    7/28 Study 런타임(언체크) 예외로 변경하기

--- src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -11,23 +11,23 @@ import java.sql.SQLException;
 public class UncheckedAppTest {
 
     @Test
-    void Unchecked(){
+    void Unchecked() {
         Controller controller = new Controller();
         Assertions.assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(RuntimeSQLException.class);
     }
 
     @Test
-    void printEx(){
+    void printEx() {
         Controller controller = new Controller();
-        try{
+        try {
             controller.request();
-        }catch (Exception e){
+        } catch (Exception e) {
             log.info("ex", e);
         }
     }
 
-    static class Controller{
+    static class Controller {
         Service service = new Service();
 
         public void request() throws SQLException, ConnectException {
@@ -35,24 +35,24 @@ public class UncheckedAppTest {
         }
     }
 
-    static class Service{
+    static class Service {
         Repository repository = new Repository();
         NetworkClient networkClient = new NetworkClient();
 
-        public void logic(){
+        public void logic() {
             repository.call();
             networkClient.call();
         }
     }
 
-    static class NetworkClient{
-        public void call(){
+    static class NetworkClient {
+        public void call() {
             throw new RuntimeConnectionException("연결 실패");
-       }
+        }
     }
 
-    static class Repository{
-        public void call(){
+    static class Repository {
+        public void call() {
             try {
                 runSQL();
             } catch (SQLException e) {
@@ -65,13 +65,13 @@ public class UncheckedAppTest {
         }
     }
 
-    static class RuntimeConnectionException extends RuntimeException{
+    static class RuntimeConnectionException extends RuntimeException {
         public RuntimeConnectionException(String message) {
             super(message);
         }
     }
 
-    static class RuntimeSQLException extends RuntimeException{
+    static class RuntimeSQLException extends RuntimeException {
         public RuntimeSQLException() {
         }
 
